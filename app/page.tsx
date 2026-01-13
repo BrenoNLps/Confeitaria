95ce4765b52b747d4d87e4f72488737a266f7f08
brenonlps <brenolopes.xy@gmail.com>
2026-01-13 09:23:42 -0300

    feat: add sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex h-screen items-center justify-center bg-pink-200">
-      <h1 className="text-5xl font-bold text-pink-800">
-        Hello World!
-      </h1>
+    <main className="scroll-smooth">
+      {/* Main */}
+      <section className="h-screen w-screen flex items-center justify-center bg-pink-200">
+        <h1 className="text-5xl font-bold text-pink-800">Bem-vindo à Confeitaria</h1>
+      </section>
+
+      {/* Produtos */}
+      <section className="h-screen w-screen flex items-center justify-center bg-yellow-200">
+        <h2 className="text-4xl font-bold text-yellow-800">Produtos</h2>
+      </section>
+
+      {/* Contato */}
+      <section className="h-screen w-screen flex items-center justify-center bg-green-200">
+        <h2 className="text-4xl font-bold text-green-800">Contato</h2>
+      </section>
     </main>
+
   );
 }
