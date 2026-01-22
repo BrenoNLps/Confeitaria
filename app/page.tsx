89fb69861329df69c735d0ba973011eb6abea325
brenonlps <brenolopes.xy@gmail.com>
2026-01-22 19:26:38 -0300

    fix: corrige page.tsx para o padrao react

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,14 @@
-import Image from "next/image";
+import Hero from "./components/sections/Hero/Hero";
+import { Products } from "./components/sections/Products/Products";
+import { About } from "./components/sections/About/About";
 
 export default function Home() {
   return (
-    <main className="scroll-smooth">
-      {/* Main */}
-      <section className="h-screen w-screen flex items-center justify-center bg-pink-200">
-        <h1 className="text-5xl font-bold text-pink-800">Bem-vindo à Confeitaria</h1>
-      </section>
-
-      {/* Produtos */}
-      <section className="h-screen w-screen flex items-center justify-center bg-yellow-200">
-        <h2 className="text-4xl font-bold text-yellow-800">Produtos</h2>
-      </section>
-
-      {/* Contato */}
-      <section className="h-screen w-screen flex items-center justify-center bg-green-200">
-        <h2 className="text-4xl font-bold text-green-800">Contato</h2>
-      </section>
-    </main>
+    <>
+      <Hero/>
+      <Products/>
+      <About/>
+    </>
 
   );
 }
