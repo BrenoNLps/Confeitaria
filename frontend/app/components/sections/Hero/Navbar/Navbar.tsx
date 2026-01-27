df71128f2f2376a72d5f965f476af3cf28810632
brenonlps <brenolopes.xy@gmail.com>
2026-01-27 15:42:14 -0300

    style: Remove borda

--- frontend/app/components/sections/Hero/Navbar/Navbar.tsx
+++ frontend/app/components/sections/Hero/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Nav() { 
     return (
-        <nav className="h-[10%] w-screen border-b-5 border-t-5 border-pink-600">
+        <nav className="h-[10%] w-screen hidden md:block">
             <ul className="h-full w-full flex justify-center items-center gap-10 text-black font-bold text-lg">
                 <li><a href="#produtos" >Produtos</a></li>
                 <li><a href="#contato" >Contato</a></li>
