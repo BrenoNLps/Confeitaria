0b3a55c777e29fcd9db752546257218e4b322340
brenonlps <brenolopes.xy@gmail.com>
2026-01-27 15:40:32 -0300

    style: Altera cor de fundo

--- frontend/app/components/sections/Hero/Main_/Article.tsx
+++ frontend/app/components/sections/Hero/Main_/Article.tsx
@@ -1,6 +1,6 @@
 export default function Article () {     
     return (
-        <article className="w-full h-[22%]  md:w-[55%] md:h-full flex flex-col justify-center items-center bg-black" id="main_text">
+        <article className="w-full h-[22%]  md:w-[55%] md:h-full flex flex-col justify-center items-center bg-white md:bg-transparent" id="main_text">
             <h1 className="title text-pink-600 ">Confeitaria artesanal</h1>
             <h2 className="title text-black ">feita com sabor, cuidado e qualidade</h2>
             
