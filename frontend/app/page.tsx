492d6b107f1d2020c321e513bdff6b10eb5ad1ee
brenonlps <brenolopes.xy@gmail.com>
2026-01-27 13:51:54 -0300

    fix: Retira sections da main

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,9 @@
 import Hero from "./components/sections/Hero/Hero";
-import { Products } from "./components/sections/Products/Products";
-import { About } from "./components/sections/About/About";
 
 export default function Home() {
   return (
     <>
       <Hero/>
-      <Products/>
-      <About/>
     </>
 
   );
