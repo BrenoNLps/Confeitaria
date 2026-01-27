460899afd646b212c24b384b7aaa85c53fd69f7e
brenonlps <brenolopes.xy@gmail.com>
2026-01-27 10:34:01 -0300

    feat: implementa container para exibir produtos

--- app/components/sections/Products/Products.tsx
+++ app/components/sections/Products/Products.tsx
@@ -1,7 +1,10 @@
+import Container from "./products_container/Container";
+
+
 export function Products() {
     return (
     <section className="h-screen w-screen flex items-center justify-center bg-yellow-200">
-        <h2 className="text-4xl font-bold text-yellow-800">Produtos</h2>
+        <Container/>
     </section>
     );
 }   
---
+++ app/components/sections/Products/products_container/Container.tsx
@@ -0,0 +1,7 @@
+export default function Container() {
+    return (
+    <section className="h-[90%] w-[90%] flex items-center justify-center bg-yellow-900">
+        <h2 className="text-4xl font-bold text-yellow-800">Produtos</h2>
+    </section>
+    );
+}   
