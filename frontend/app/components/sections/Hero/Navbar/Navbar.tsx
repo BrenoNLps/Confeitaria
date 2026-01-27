45147c9e37bc65ee1eafd165bb8efba86b60955a
brenonlps <brenolopes.xy@gmail.com>
2026-01-27 13:47:03 -0300

    style: altera cor de borda do nav

--- frontend/app/components/sections/Hero/Navbar/Navbar.tsx
+++ frontend/app/components/sections/Hero/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Nav() { 
     return (
-        <nav className="h-[10%] w-screen border-b-5 border-black bg-stone-100">
+        <nav className="h-[10%] w-screen border-b-5 border-t-5 border-pink-600">
             <ul className="h-full w-full flex justify-center items-center gap-10 text-black font-bold text-lg">
                 <li><a href="#produtos" >Produtos</a></li>
                 <li><a href="#contato" >Contato</a></li>
