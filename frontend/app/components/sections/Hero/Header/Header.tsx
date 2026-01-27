01ee35a7808dbd765b46ce7e716e0f3da870f134
brenonlps <brenolopes.xy@gmail.com>
2026-01-27 15:36:15 -0300

    style: Adicao de margem e borda

--- frontend/app/components/sections/Hero/Header/Header.tsx
+++ frontend/app/components/sections/Hero/Header/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <header className="h-[10%] w-screen flex items-center justify-center md:justify-start text-black text-4xl font-bold">
-                <p className="ml-2">Mari</p>
+        <header className="h-[10%] w-screen flex items-center justify-center md:justify-start text-black text-4xl font-bold border-b-5  border-black">
+                <p className="md:ml-20 ml-0">&nbsp;Mari</p>
                 <img src="images/logo_bolo.png" alt="bolo_logo" className=" h-full" />
                 <p>Lopes</p>
         </header>
