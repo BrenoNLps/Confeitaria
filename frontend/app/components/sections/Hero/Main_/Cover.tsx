3a9edc5d58f3464f8fc0987bf4d1e4aba252abc7
brenonlps <brenolopes.xy@gmail.com>
2026-01-28 11:44:17 -0300

    style: Alteracao de tamanho, posicao  e adicao de tag button

--- frontend/app/components/sections/Hero/Main_/Cover.tsx
+++ frontend/app/components/sections/Hero/Main_/Cover.tsx
@@ -1,7 +1,13 @@
-export default function cover() {
+
+export default function Cover() {
     return (
-        <div className="w-full md:w-[30%] h-[50%] md:h-[80%] flex justify-center  ">
-            <img src="images/bolo.png" alt="Bolo" className="h-full object-cover" />
+        <div className="w-full h-[70%] md:h-full md:flex-1 justify-start md:justify-center items-center flex flex-col ">
+            <div className=" w-[70%] md:w-[60%] h-[50%] md:h-[80%] justify-center items-center flex">
+                <img src="images/bolo.png" alt="Bolo" className="h-full w-full object-cover md:rounded-none rounded-2xl border-5 border-black" />
+            </div>
+
+            <button className="mt-6 w-[70%] md:w-[30%] h-12 font-bold rounded-2xl bg-black text-white hover:bg-pink-800 transition-colors block md:hidden"> Cardápio
+            </button>
         </div>  
     );
 }
