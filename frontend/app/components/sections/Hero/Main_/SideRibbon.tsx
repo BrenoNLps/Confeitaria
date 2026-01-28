06c08325880fb8fb7a20161c70849d67ba4ad118
brenonlps <brenolopes.xy@gmail.com>
2026-01-28 11:42:25 -0300

    style: Alteracao de largura da faixa

--- frontend/app/components/sections/Hero/Main_/SideRibbon.tsx
+++ frontend/app/components/sections/Hero/Main_/SideRibbon.tsx
@@ -1,5 +1,5 @@
 export default function SideRibbon() {
     return (
-        <div className="h-full w-[30%] bg-pink-600  right-0 top-0 absolute"/>
+        <div className="h-full w-[25%] md:w-[25%] bg-pink-600  right-0 top-0 absolute"/>
     );
 }
