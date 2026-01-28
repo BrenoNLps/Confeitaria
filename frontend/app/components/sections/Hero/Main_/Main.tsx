c6d7b43615b62345d1208d274b82c7b9e3424272
brenonlps <brenolopes.xy@gmail.com>
2026-01-28 11:47:07 -0300

    style: Alteracao de tamanho e remocao de button

--- frontend/app/components/sections/Hero/Main_/Main.tsx
+++ frontend/app/components/sections/Hero/Main_/Main.tsx
@@ -4,15 +4,13 @@ import SideRibbon from "./SideRibbon";
 
 export default function Main () {         
     return (
-        <main className="h-[80%] w-screen relative flex items-center">
-            <SideRibbon />
-            
+        <main className="h-[90%] md:h-[80%] w-screen relative flex items-center ">
+
             <div className="w-screen h-full flex flex-col md:flex-row items-center relative z-10" id="main_content">
                 <Article/>
                 <Cover/>
-                <button className="md:ml-10 ml-2 mt-6 md:mt-10 w-[60%] md:w-[30%] h-12 font-bold rounded-3xl bg-pink-600 text-white hover:bg-pink-800 transition-colors block md:hidden"> Ver Produtos
-                </button>
             </div>
+            <SideRibbon />
         </main>
     );
 }
