877e5ba90292813f24e4ad5e049b967588b03e3f
brenonlps <brenolopes.xy@gmail.com>
2026-01-29 19:35:57 -0300

    feat: Adiciona logo svg e favicon ico

--- frontend/app/components/common/Header.tsx
+++ frontend/app/components/common/Header.tsx
@@ -1,9 +1,9 @@
 export default function Header() {
     return (
         <header className="h-[15%] md:h-[10%] w-screen  md:border-b-4 border-b-none flex items-center justify-center border-black">
-            <div className="w-[87.5%] h-full flex items-center justify-center md:justify-start">
+            <div className="w-[87.5%] h-full flex items-end justify-center md:justify-start">
                 <a href="/" className="h-[80%]">
-                    <img src="images/logo_bolo.png" alt="bolo_logo" className="h-full"/>
+                    <img src="images/logo.svg" alt="bolo_logo" className="h-full"/>
                 </a>
             </div>
         </header>
