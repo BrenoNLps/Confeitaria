c3a9104b93ce80f00ac6f8ce81d04e82719dd8d9
brenonlps <brenolopes.xy@gmail.com>
2026-01-29 12:10:17 -0300

    feat: Retira texto  da logo e transforma logo inteira em imagem

--- frontend/app/components/common/Header.tsx
+++ frontend/app/components/common/Header.tsx
@@ -1,12 +1,11 @@
 export default function Header() {
     return (
-        <header className="h-[15%] md:h-[10%] w-screen  text-[1.6rem] md:text-4xl font-bold border-b-5 flex items-center justify-center border-black text-black ">
+        <header className="h-[15%] md:h-[10%] w-screen  md:border-b-4 border-b-none flex items-center justify-center border-black">
             <div className="w-[87.5%] h-full flex items-center justify-center md:justify-start">
-                <p>Mari L</p>
-                <img src="images/logo_bolo.png" alt="bolo_logo" className=" h-full" />
-                <p>pes</p>
+                <a href="/" className="h-[80%]">
+                    <img src="images/logo_bolo.png" alt="bolo_logo" className="h-full"/>
+                </a>
             </div>
-
         </header>
     );
 }
