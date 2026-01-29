8206efeb01da32db17ff954a144a8ab83de27b1b
brenonlps <brenolopes.xy@gmail.com>
2026-01-29 07:36:40 -0300

    style: Atualiza estilos do componente Article e remove cor de fundo desnecessária

--- frontend/app/components/hero/Article.tsx
+++ frontend/app/components/hero/Article.tsx
@@ -1,10 +1,10 @@
 export default function Text () {     
     return (
-        <article className="w-full h-[30%] md:flex-1 md:h-full flex flex-col justify-center items-center bg-white" id="main_text">
+        <article className="w-full h-[30%] md:flex-1 md:h-full flex flex-col justify-center items-center bg-black md:bg-transparent" id="main_text">
             <div className="w-[70%]  flex flex-col justify-center md:items-start items-center">   
-                <h1 className="title text-pink-600 ">Confeitaria da Mari</h1>
-                <h2 className="title text-black ">Feito com sabor, cuidado e qualidade.</h2>
-                <button className="text-4xl mt-6 md:mt-10  font-bold rounded-4xl border-3 border-black bg-pink-600 text-white hover:bg-pink-800 transition-colors hidden md:block p-3 pl-5 pr-5">Ver Cardápio
+                <h1 className="title text-white md:text-black ">Confeitaria da Mari</h1>
+                <h2 className="title text-pink-600 ">Feito com sabor, cuidado e qualidade.</h2>
+                <button className="text-4xl mt-6 md:mt-10  font-bold rounded-4xl border-3 border-black bg-transparent text-black hover:bg-pink-800 transition-colors hidden md:block p-3 pl-7 pr-7">Ver Cardápio
                 </button>
             </div>
         </article>
