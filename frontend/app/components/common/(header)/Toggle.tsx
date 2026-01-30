29516155ec6b534f4b7d5c4e905c652d085ae756
brenonlps <brenolopes.xy@gmail.com>
2026-01-30 11:40:49 -0300

    style: Alteracao de cores

--- frontend/app/components/common/(header)/Toggle.tsx
+++ frontend/app/components/common/(header)/Toggle.tsx
@@ -1,6 +1,7 @@
 export default function Toggle(){
     return(
-        <button className="h-[70%] aspect-square bg-pink-400 justify-center items-center md:hidden flex rounded-br-xl rounded-tr-xl">
+        <button 
+        className="h-[70%] aspect-square bg-rose-400 justify-center items-center md:hidden flex rounded-br-xl rounded-tr-xl text-white text-3xl">
             &#9776;
         </button>
     );
