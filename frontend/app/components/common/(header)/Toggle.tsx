9532315062e571bad552ad53eebae20aa21d1e6a
brenonlps <brenolopes.xy@gmail.com>
2026-01-30 08:57:16 -0300

    feat: Cria toggle

---
+++ frontend/app/components/common/(header)/Toggle.tsx
@@ -0,0 +1,7 @@
+export default function Toggle(){
+    return(
+        <button className="h-[70%] aspect-square bg-pink-400 justify-center items-center md:hidden flex rounded-br-xl rounded-tr-xl">
+            &#9776;
+        </button>
+    );
+}
