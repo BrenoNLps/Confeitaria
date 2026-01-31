e85f505f21df1ffa0572c5b64510f6939044d546
brenonlps <brenolopes.xy@gmail.com>
2026-01-31 16:55:42 -0300

    feat: Adiciona container ao main da pagina galeria

---
+++ frontend/app/(pages)/gallery/page.tsx
@@ -0,0 +1,9 @@
+import Container from "@/app/components/(pages)/gallery/Container";
+
+export default function Gallery() {
+    return (
+        <div className=" h-[90%] md:h-[80%] w-screen flex items-center ">
+            <Container/>
+        </div>
+    );
+}
