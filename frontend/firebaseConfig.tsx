35e4bd37054cc8600a8f677d21d5cfaaf015758b
brenonlps <brenolopes.xy@gmail.com>
2026-02-01 17:31:48 -0300

    feat: Configura firebase

---
+++ frontend/firebaseConfig.tsx
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+
+interface FirebaseConfig {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+    measurementId?: string;
+}
+
+const firebaseConfig: FirebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID as string,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
+const storage = getStorage(app);
+export { app, analytics, storage };
