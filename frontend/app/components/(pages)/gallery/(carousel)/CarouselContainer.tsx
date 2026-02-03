c18d13907330025ec464c8b3155b5381fe30ec86
brenonlps <brenolopes.xy@gmail.com>
2026-02-03 16:49:07 -0300

    style: Remove cor do container corrossel

--- frontend/app/components/(pages)/gallery/(carousel)/CarouselContainer.tsx
+++ frontend/app/components/(pages)/gallery/(carousel)/CarouselContainer.tsx
@@ -3,7 +3,7 @@ import Carousel from './Carousel';
 
 export default function CarouselContainer(){
     return(
-        <div className="w-[90%] h-[90%] bg-blue-200 bg-amber-150 flex items-center justify-center relative">
+        <div className="w-[90%] h-[90%] bg-amber-150 flex items-center justify-center relative">
 
             <button className="absolute left-0 text-white text-9xl drop-shadow-[-1px_3px_3px_black] z-10 hover:text-black transition">&#x2039;</button>
             <Carousel/>
