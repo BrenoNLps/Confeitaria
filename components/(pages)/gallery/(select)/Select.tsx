62e5139c9fdcdaceb95359c85e343e71dd058910
brenonlps <brenolopes.xy@gmail.com>
2026-02-04 15:31:53 -0300

    feat: Adiciona onChange value ao select e define tipos como enum

--- components/(pages)/gallery/(select)/Select.tsx
+++ components/(pages)/gallery/(select)/Select.tsx
@@ -1,16 +1,34 @@
-export default function Select(){
+"use client"
+export enum cakeType {
+    COMMEMORATIVE = "commemorative",
+    MONTHLY = "monthly",
+    SIMPLE = "simple",
+    SPECIAL = "special",
+    THEMATIC = "thematic",
+}
+
+type Props = {
+    value : cakeType;
+    onChange: (v: cakeType) => void;
+}
+
+export default function Select({ value, onChange }: Props){
     return (
         <div className="w-full h-[10%] flex justify-center items-center " >
-            <select className="h-full w-[90%] bg-black text-white text-center bord-0 appearance-none hover:bg-pink-500 hover:text-white 
-            focus:outline-0 
-            ">
-                <option value="" disabled selected hidden>Escolha     ▼</option>
-                <option value="Tematico">Temático</option>
-                <option value="Simples">Simples</option>
-                <option value="Especial">Especial</option>
-                <option value="MesVersario">MesVersário</option>
-                <option value="Comemorativo">Comemorativo</option>
+            <select 
+            value={value} 
+            onChange={(e) => onChange(e.target.value as cakeType)}
+            className="h-full w-[90%] bg-black text-white text-center bord-0 appearance-none hover:bg-rose-400 hover:text-white focus:outline-0 } "
+
+            >
+                <option value="" disabled selected hidden>Escolha ▼</option>
+                <option value={cakeType.THEMATIC}>Temático</option>
+                <option value={cakeType.SIMPLE}>Simples</option>
+                <option value={cakeType.SPECIAL}>Especial</option>
+                <option value={cakeType.MONTHLY}>MesVersário</option>
+                <option value={cakeType.COMMEMORATIVE}>Comemorativo</option>
             </select>
+            
         </div>
     );
 }
