aa0ba262aa95edaf36c2d3e9087fa21da92b9d9d
brenonlps <brenolopes.xy@gmail.com>
2026-02-05 20:42:58 -0300

    chore: Alteracao de identacao,  nome de variavel,  posicao inicial de module de style etc...

--- components/(pages)/gallery/(carousel)/Carousel.tsx
+++ components/(pages)/gallery/(carousel)/Carousel.tsx
@@ -5,28 +5,22 @@ import { useCarousel } from "./CarouselContext";
 
 
 export default function Carousel() {
+    const CAMINHO_GALERIA_FIREBASE = "public/gallery/cakes/"
     const { categoria, index, imgWidth } = useCarousel();
-    const { data: images, error, loading } = useFirebase(
-        `public/gallery/cakes/${categoria}`
-    );
+    const { data: images, error, loading } = useFirebase(`${CAMINHO_GALERIA_FIREBASE}${categoria}`);
     
     return (
-        <div className="overflow-hidden w-[25rem] md:w-[76rem] h-full flex items-center justify-center">
-        {error && <p className="text-red-500">{error}</p>}
-        <ul className={styles.carousel}>
-
-            {!loading && images.length > 0 ? (images.map((url, index) => (
-                <li key={index}>
-                    <img src={url} alt={`Imagem ${index + 1}`} />
-                </li>)))
-            :(
-                <>
-                <li><img src="/images/carregando_bolo.gif" alt="Fallback 1" /></li>
-                <li><img src="/images/carregando_bolo.gif" alt="Fallback 2" /></li>
-                <li><img src="/images/carregando_bolo.gif" alt="Fallback 3" /></li>
-                </>
-            )}
-        </ul>
+        <div className={styles.carousel}>
+            {error && <p className="text-red-500">{error}</p>}
+            <ul >
+                {!loading && images.length > 0 ? (images.map((url, i) => (
+                <li key={url}><img src={url} alt={`Imagem ${i + 1}`} /></li>))) : 
+                (<>
+                    <li><img src="/images/carregando_bolo.gif" alt="Fallback 1" /></li>
+                    <li><img src="/images/carregando_bolo.gif" alt="Fallback 2" /></li>
+                    <li><img src="/images/carregando_bolo.gif" alt="Fallback 3" /></li>
+                </>)}
+            </ul>
         </div>
     );
 }
