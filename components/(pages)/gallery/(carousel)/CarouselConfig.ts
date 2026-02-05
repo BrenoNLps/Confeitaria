648021936838e00e15946610f474a282f52986c7
brenonlps <brenolopes.xy@gmail.com>
2026-02-05 20:19:38 -0300

    feat: Cria config carousel para colocar dados padroes do carrossel

---
+++ components/(pages)/gallery/(carousel)/CarouselConfig.ts
@@ -0,0 +1,11 @@
+export enum cakeType {
+    COMMEMORATIVE = "commemorative",
+    MONTHLY = "monthly",
+    SIMPLE = "simple",
+    SPECIAL = "special",
+    THEMATIC = "thematic",
+}
+
+export const imgSizeDesktop = 25;
+export const imgSizeMobile = 18;
+export const marginSize = 0.5;
