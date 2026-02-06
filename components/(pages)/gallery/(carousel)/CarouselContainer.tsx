08af5f7a0200f2577c8860207ab050f848938853
brenonlps <brenolopes.xy@gmail.com>
2026-02-05 21:41:05 -0300

    style: Adiciona button ao modulo carrossel

--- components/(pages)/gallery/(carousel)/CarouselContainer.tsx
+++ components/(pages)/gallery/(carousel)/CarouselContainer.tsx
@@ -1,14 +1,14 @@
 import Carousel from './Carousel';
-
+import styles from "./carousel.module.css"
 
 
 export default function CarouselContainer(){
     return(
         <div className="w-[90%] h-[90%] bg-amber-150 flex items-center justify-center relative">
 
-            <button className="absolute left-0 text-white text-9xl drop-shadow-[-1px_3px_3px_black] z-10 hover:text-black transition">&#x2039;</button>
+            <button className={`left-0 ${styles.button}`}>&#x2039;</button>
             <Carousel />
-            <button className="absolute right-0 text-white text-9xl drop-shadow-[1px_3px_3px_black] z-10 hover:text-black transition">&#x203A;</button>
+            <button className={`right-0 ${styles.button}`}>&#x203A;</button>
             
         </div>
     );
