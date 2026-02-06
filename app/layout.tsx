e80c659affabb45476e8fa4c78d07c9040fa8208
brenonlps <brenolopes.xy@gmail.com>
2026-02-06 15:46:09 -0300

    fix: Muda local do navbar e o aloca dentro do header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/common/(header)/Header";
-import Nav from "../components/common/Navbar";
+import Nav from "../components/common/(header)/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Header />
-        <Nav/>
         {children}
       </body>
     </html>
