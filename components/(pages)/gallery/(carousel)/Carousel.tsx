b0cba1308df54e176a629f22c02468abfbef6a39
brenonlps <brenolopes.xy@gmail.com>
2026-02-06 12:43:33 -0300

    feat: Adiciona controle de largura de imagem e total de imagens no carrossel

--- components/(pages)/gallery/(carousel)/Carousel.tsx
+++ components/(pages)/gallery/(carousel)/Carousel.tsx
@@ -2,10 +2,12 @@
 import styles from "./carousel.module.css";
 import { useFirebase } from "@/hooks/useFirebase";
 import { useCarousel } from "./CarouselContext";
+import { imgSizeDesktop, imgSizeMobile, INDICE_CARROSSEL_DESKTOP_NEXT_DESK, INDICE_CARROSSEL_DESKTOP_NEXT_MOBI, marginSize } from "./CarouselConfig";
+import { useEffect } from "react";
 
 
 export default function Carousel() {
-    const { categoria, index, imgWidth }  = useCarousel();
+    const { categoria, index, imgWidth,setImgWidth,totalImgs, setTotalImgs }  = useCarousel();
     const CAMINHO_FIREBASE_GALERIA = `public/gallery/cakes/${categoria}`;
     const { data: images, error, loading } = useFirebase(CAMINHO_FIREBASE_GALERIA);
 
@@ -14,20 +16,18 @@ export default function Carousel() {
 
 
     return (
-        <div className={styles.carousel}>
+        <div className={styles.carousel} >
             {error && <p className="text-red-500">{error}</p>}
-            <ul> 
-            {
-                !loading && images.length > 0 ? (images.map((url, i) => (
+            <ul style={{transform: `translateX(-${index * (imgWidth + marginSize)}rem)`}}> 
+                {!loading && images.length > 0 ? (images.map((url, i) => (
                     <li key={url}><img src={url} alt={`Imagem ${i + 1}`} /></li>
-                ))) : (
-                    <>
+                ))) : 
+                (<>
                     <li><img src="/images/carregando_bolo.gif" alt="Fallback 1" /></li>
                     <li><img src="/images/carregando_bolo.gif" alt="Fallback 2" /></li>
                     <li><img src="/images/carregando_bolo.gif" alt="Fallback 3" /></li>
-                    </>
-                )
-            }
+                </>)
+                }
             </ul>
         </div>
     );
