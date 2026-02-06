13af0b8e7c1d49d71e543380eef29b6a4d9c79f1
brenonlps <brenolopes.xy@gmail.com>
2026-02-06 16:23:55 -0300

    style: Altera cor e adiciona efeito hover

--- components/common/(header)/Toggle.tsx
+++ components/common/(header)/Toggle.tsx
@@ -2,7 +2,7 @@ export default function Toggle({ onToggle }: { onToggle: () => void }){
     return(
         <button 
         onClick={onToggle}
-        className="h-[70%] aspect-square bg-rose-400 justify-center items-center md:hidden flex rounded-br-xl rounded-tr-xl text-white text-3xl z-4">
+        className="h-[70%] aspect-square bg-black justify-center items-center md:hidden flex rounded-br-xl rounded-tr-xl text-white text-3xl z-4 hover:bg-rose-400">
             &#9776;
         </button>
     );
