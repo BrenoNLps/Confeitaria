dbac87d8cdd1b1ea9a1b9c97c1516d1a7c1fd1fc
brenonlps <brenolopes.xy@gmail.com>
2026-02-06 12:35:49 -0300

    feat: Adiciona variavel para guardar total de imagens do carrossel

--- components/(pages)/gallery/(carousel)/CarouselContext.tsx
+++ components/(pages)/gallery/(carousel)/CarouselContext.tsx
@@ -9,15 +9,17 @@ type CarouselContextType = {
     categoria : cakeType ; setCategoria : (c: cakeType) => void;
     index     : number   ; setIndex     : (i: number  ) => void;
     imgWidth  : number   ; setImgWidth  : (w: number  ) => void;
+    totalImgs : number   ; setTotalImgs : (w: number  ) => void;
 };
 
 export function CarouselProvider({ children }: { children: React.ReactNode }) {
     const [categoria, setCategoria ] = useState(cakeType.SIMPLE);
     const [index    , setIndex     ] = useState(       0       );
     const [imgWidth , setImgWidth  ] = useState( imgSizeMobile );
+    const [totalImgs, setTotalImgs ] = useState(       0       );
 
     return (
-        <CarouselContext.Provider value={{ categoria, setCategoria, index, setIndex, imgWidth, setImgWidth }}>
+        <CarouselContext.Provider value={{ categoria, setCategoria, index, setIndex, imgWidth, setImgWidth,totalImgs, setTotalImgs }}>
             {children}
         </CarouselContext.Provider>
     );
