7b841678bc0c9cebdd4a422cb4f97ceb89f47bbe
brenonlps <brenolopes.xy@gmail.com>
2026-02-05 21:23:11 -0300

    chore: Alteracoes de identacao

--- components/(pages)/gallery/(carousel)/Carousel.tsx
+++ components/(pages)/gallery/(carousel)/Carousel.tsx
@@ -5,21 +5,25 @@ import { useCarousel } from "./CarouselContext";
 
 
 export default function Carousel() {
-    const CAMINHO_GALERIA_FIREBASE = "public/gallery/cakes/"
-    const { categoria, index, imgWidth } = useCarousel();
-    const { data: images, error, loading } = useFirebase(`${CAMINHO_GALERIA_FIREBASE}${categoria}`);
+    const { categoria, index, imgWidth }  = useCarousel();
+    const CAMINHO_FIREBASE_GALERIA = `public/gallery/cakes/${categoria}`;
+    const { data: images, error, loading } = useFirebase(CAMINHO_FIREBASE_GALERIA);
     
     return (
         <div className={styles.carousel}>
             {error && <p className="text-red-500">{error}</p>}
-            <ul >
-                {!loading && images.length > 0 ? (images.map((url, i) => (
-                <li key={url}><img src={url} alt={`Imagem ${i + 1}`} /></li>))) : 
-                (<>
+            <ul> 
+            {
+                !loading && images.length > 0 ? (images.map((url, i) => (
+                    <li key={url}><img src={url} alt={`Imagem ${i + 1}`} /></li>
+                ))) : (
+                    <>
                     <li><img src="/images/carregando_bolo.gif" alt="Fallback 1" /></li>
                     <li><img src="/images/carregando_bolo.gif" alt="Fallback 2" /></li>
                     <li><img src="/images/carregando_bolo.gif" alt="Fallback 3" /></li>
-                </>)}
+                    </>
+                )
+            }
             </ul>
         </div>
     );
