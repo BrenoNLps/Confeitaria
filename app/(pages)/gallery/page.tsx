71d0fdd75cf9292c7a5a4062d399f641c7d287b1
BrenoNLps <brenolopes.xy@gmail.com>
2026-02-11 05:24:09 -0300

    fix: Ajusta nome da div principal da galeria para main

--- app/(pages)/gallery/page.tsx
+++ app/(pages)/gallery/page.tsx
@@ -2,8 +2,8 @@ import Container from "@/components/pages/gallery/Container";
 
 export default function Gallery() {
   return (
-    <div className=" h-[90%] md:h-[80%] w-screen flex items-center ">
+    <main>
       <Container />
-    </div>
+    </main>
   );
 }
