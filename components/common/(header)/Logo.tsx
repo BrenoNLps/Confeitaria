c757fd02afcd441b314d9c84582d939ee1fe2c85
BrenoNLps <brenolopes.xy@gmail.com>
2026-02-11 04:51:10 -0300

    fix: Substitui a tag do logo de img para Image

--- components/common/(header)/Logo.tsx
+++ components/common/(header)/Logo.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Logo(){
     return(
         <div className=" w-full h-[70%] md:h-[50%]  md:border-b-4 border-b-none  flex justify-center md:justify-start ">
                 <Link href="/" className=" ml-0 md:ml-[8%] flex justify-end items-center md:justify-start ">
-                    <img src="images/logo.svg" alt="bolo_logo" className="h-11/12 w-50 md:w-60"/>
+                    <Image src="images/logo.svg" alt="bolo_logo"  width={240} height={48} className="h-11/12 w-50 md:w-60"/>
                 </Link>
         </div>        
     );
