bbf6269de0a3cca2c93939ff303830b28a02cae6
BrenoNLps <brenolopes.xy@gmail.com>
2026-02-14 05:05:16 -0300

    refactor: Remove  consts que nao serao mais usadas e adiciona const de categorias

--- components/pages/gallery/carousel/CarouselConfig.ts
+++ components/pages/gallery/carousel/CarouselConfig.ts
@@ -6,12 +6,10 @@ export enum cakeType {
     THEMATIC      = "thematic"     ,
 }
 
-export const imgSizeDesktop = 25  ;
-export const imgSizeMobile  = 18  ;
-export const marginSize     = 0.5 ;
-
-
-//Para carrossel nao ultrapassar o rolamento a direita
-export const INDICE_CARROSSEL_DESKTOP_PREV      = 0;
-export const INDICE_CARROSSEL_DESKTOP_NEXT_MOBI = 1;
-export const INDICE_CARROSSEL_DESKTOP_NEXT_DESK = 3;
+export const categorias = [
+    { tipo: cakeType.SIMPLE, label: "Simples" },
+    { tipo: cakeType.COMMEMORATIVE, label: "Comemorativos" },
+    { tipo: cakeType.MONTHLY, label: "Mensais" },
+    { tipo: cakeType.SPECIAL, label: "Especiais" },
+    { tipo: cakeType.THEMATIC, label: "Temáticos" },
+];
