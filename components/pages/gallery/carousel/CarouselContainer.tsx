f3c3d086c92ae59eab5dd7ceee6f1aa6c26b5eb8
BrenoNLps <brenolopes.xy@gmail.com>
2026-02-14 05:45:10 -0300

    refactor: Insere botoes para selecionar a categoria de bolo ao inves do antigo select

--- components/pages/gallery/carousel/CarouselContainer.tsx
+++ components/pages/gallery/carousel/CarouselContainer.tsx
@@ -1,23 +1,25 @@
+"use client";
+import { useState } from "react";
 import Carousel from './Carousel';
-import styles from "./carousel.module.css"
-import { INDICE_CARROSSEL_DESKTOP_PREV } from './CarouselConfig';
-import { useCarousel } from './CarouselContext';
+import { cakeType, categorias } from './CarouselConfig';
 
 
 
 export default function CarouselContainer(){
-    const { setIndex,totalImgs } = useCarousel();
-
-    //const next = () => setIndex((i) => (i + 1));
-    const next = () => setIndex((i) => Math.min(i + 1, totalImgs));
-    const prev = () => setIndex((i) => Math.max(i - 1, INDICE_CARROSSEL_DESKTOP_PREV ));
+    const [categoria, setCategoria] = useState<cakeType>(cakeType.SIMPLE);
 
     return(
-        <div className="w-[90%] h-[90%]">
-            <div className='w-full h-full flex items-center justify-center relative '>
-                <button onClick={prev} className={`left-0 ${styles.button}`}>&#x2039;</button>
-                <Carousel />
-                <button onClick={next} className={`right-0 ${styles.button}`}>&#x203A;</button>
+        <div className="w-full max-w-7xl mx-auto px-4">
+            <div className="flex gap-2 mb-4 justify-center flex-wrap ">
+                {categorias.map(({ tipo, label }) => (
+                    <button 
+                        className={`px-4 py-2 rounded transition-colors ${categoria === tipo ? 'bg-rose-400 text-white' : 'bg-gray-300 hover:bg-gray-400 text-gray-700'}`}
+                        key={tipo} 
+                        onClick={() => setCategoria(tipo)}
+                    >
+                        {label}
+                    </button>
+                ))}
             </div>
         </div>
     );
