c7433606bee78f06d24ea3301bf6248c89d4b330
BrenoNLps <brenolopes.xy@gmail.com>
2026-02-14 05:03:13 -0300

    refactor: Remove Imports do container

--- components/pages/gallery/Container.tsx
+++ components/pages/gallery/Container.tsx
@@ -1,16 +1,11 @@
 "use client";
 
 import CarouselContainer from "./carousel/CarouselContainer";
-import Select from "./select/Select";
-import { CarouselProvider } from "./carousel/CarouselContext";
 
 export default function Container() {
   return (
-    <CarouselProvider>
       <section className="flex-col">
-        <Select />
         <CarouselContainer />
       </section>
-    </CarouselProvider>
   );
 }
