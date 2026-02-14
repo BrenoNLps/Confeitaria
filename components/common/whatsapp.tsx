6d11464a6ff356b66049f31a305bf8e063a1b657
BrenoNLps <brenolopes.xy@gmail.com>
2026-02-14 06:53:17 -0300

    style: Adiciona z-index ao botão do WhatsApp para melhorar a sobreposição

--- components/common/whatsapp.tsx
+++ components/common/whatsapp.tsx
@@ -12,7 +12,7 @@ export default function WhatsAppButton() {
             href={whatsappURL}
             target="_blank"
             rel="noopener noreferrer"
-            className="fixed bottom-6 right-6  bg-green-500 hover:bg-green-600 text-white rounded-full p-4 shadow-lg transition-all hover:scale-110"
+            className="fixed bottom-6 right-6  bg-green-500 hover:bg-green-600 text-white rounded-full p-4 shadow-lg transition-all hover:scale-110 z-50"
         >
             <FaWhatsapp className="w-8 h-8" />
         </Link>
